297706950d06f06f2225c39193345b1efecef69f
JuneBuug <junee613@gmail.com>
2017-05-31 02:29:57 +0900

    [Codility] Distinct 추가

---
+++ Distinct.java
@@ -0,0 +1,22 @@
+
+import java.util.HashMap;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i: A){
+            if(map.containsKey(i)){
+                map.put(i,map.get(i)+1);
+            }else{
+                map.put(i,1);
+            }
+        }
+        
+        return map.keySet().size();
+    }
+}
